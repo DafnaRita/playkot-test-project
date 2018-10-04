52e64f7b0c3cb5029de4697f16f87be0e8e5331e
margo <margo.sockolova2010@yandex.ru>
2018-10-04 14:58:44 +0300

    make App stateless component

--- src/App.js
+++ src/App.js
@@ -7,44 +7,31 @@ import Auth from './components/Auth';
 import Users from './components/Users';
 import sessionControlHoc from './components/hocs/sessionControlHoc';
 
-class App extends Component {
-  constructor() {
-    console.log('App start');
-    super();
-    this.state = {
-      test: 'test',
-    };
-  }
-
-  render() {
-    console.log('App render');
-    console.log('state - ', this.state);
-    console.log('props - ', this.props);
-    return (
-      <div>
-        <Route exact
-          path="/auth"
-          {...this.props}
-          render={() => <Auth {...this.props}
-            login={this.props.login}
-            logout={this.props.logout}
-          />}/>
-        <Route exact
-          path="/users"
-          {...this.props}
-          render={() => <Users {...this.props}/>}/>
-        <Route exact path='/' component={() => {
-          return this.props.isAuthenticated ? (
-            <Redirect to='/users'/>
-          ) : (
-            <Redirect to={{
-              pathname: '/auth',
-            }} />
-          );
-        }}/>
-      </div>
-    );
-  }
-}
+const App = (props) => {
+  return (
+    <div>
+      <Route exact
+        path="/auth"
+        {...props}
+        render={() => <Auth {...props}
+          login={props.login}
+          logout={props.logout}
+        />}/>
+      <Route exact
+        path="/users"
+        {...props}
+        render={() => <Users {...props}/>}/>
+      <Route exact path='/' component={() => {
+        return props.isAuthenticated ? (
+          <Redirect to='/users'/>
+        ) : (
+          <Redirect to={{
+            pathname: '/auth',
+          }} />
+        );
+      }}/>
+    </div>
+  );
+};
 
 export default sessionControlHoc(App);
