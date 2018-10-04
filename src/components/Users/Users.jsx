4f4441c0bc5c80419d9d6384f0a15b60b85a81db
margo <margo.sockolova2010@yandex.ru>
2018-10-04 14:45:39 +0300

    remove redundant routers

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,20 +6,23 @@ import styles from './Users.css';
 const Users = (props) => {
   const { isAuthenticated } = props;
   console.log('isAuthenticated - ', isAuthenticated);
-  return (
-    isAuthenticated ? (<Route exact path="/users">
+  const content = (
+    isAuthenticated ? (
       <div>
         Users(Authenticated)
         <Link to='/auth'>Link</Link>
       </div>
-    </Route>) : (<Route exact path="/users">
+    ) : (
       <div>
-        Please, Authenticated
-        <Link to='/auth'>Link</Link>
-      </div>
-    </Route>)
+        Please,
+        <Link to='/auth'>Authenticate</Link>
+      </div>)
   );
-}
-
+  return (
+    <div>
+      { content }
+    </div>
+  );
+};
 
 export default Users;
